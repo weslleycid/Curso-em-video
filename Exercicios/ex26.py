7d57541c9774f48a9df0e5a103bd8ef7f2821ff0
Weslley Alves <weslleycid@gmail.com>
2022-12-12 06:32:03 -0300

    Deu certo!!

--- Exercicios/ex26.py
+++ Exercicios/ex26.py
@@ -5,3 +5,7 @@ nomeCompleto1 = nomeCompleto.split()
 nomeCompleto2= nomeCompleto.reverse(nomeCompleto)
 print('Seu primeiro nome é {} '.format(nomeCompleto1[0]))
 print('Seu ultimo nome é {}'.format(nomeCompleto2))
+
+
+
+#acho que deu certo!!!
