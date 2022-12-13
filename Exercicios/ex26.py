3b048b001e2b75365e2156d2c645edca4c737cc0
Weslley Alves <weslleycid@gmail.com>
2022-12-13 05:44:38 -0300

    Estudando

--- Exercicios/ex26.py
+++ Exercicios/ex26.py
@@ -1,7 +1,6 @@
-nomeCompleto = input('Digite seu nome completo: ')
-
-
-nomeCompleto1 = nomeCompleto.split()
-nomeCompleto2= nomeCompleto.reverse(nomeCompleto)
-print('Seu primeiro nome é {} '.format(nomeCompleto1[0]))
-print('Seu ultimo nome é {}'.format(nomeCompleto2))
+nomeCompleto = str(input('Digite seu nome completo: ')).strip()
+nomeCompleto1 = nomeCompleto.upper()#,nomeCompleto.split(), nomeCompleto.strip()
+print(nomeCompleto1)
+print('A letra A aparece {} vezes'.format(nomeCompleto1.count('A')))
+print('A letra apareceu A apareceu a primeira vez na posição {}'.format(nomeCompleto1.find('A')+1))
+print('A letra apareceu A apareceu pela ultima vez na posição {}'.format(nomeCompleto1.rfind('A')+1))
---
+++ Exercicios/ex27.py
@@ -0,0 +1,6 @@
+nomeCompleto = input('Digite seu nome completo: ')
+
+
+nomeCompleto1 = nomeCompleto.split()
+print('Seu primeiro nome é {} '.format(nomeCompleto1[0]))
+print('Seu ultimo nome é {}'.format(nomeCompleto1[-1]))
