303b03f62193f3428d066c3d2a3b56f680eafed4
weslley.alves <weslleycid@gmail.com>
2022-12-18 10:43:05 -0300

    Exercicio 40 concluído

--- Exercicios/ex40.py
+++ Exercicios/ex40.py
@@ -3,14 +3,15 @@ print('RESULTADO FINAL')
 nota1 = float(input ('digite sua primeira nota: '))
 nota2 = float(input('Digite sua segunda nota: '))
 media = float(nota1+nota2)/2
-sexo = int(input('Digite seu sexo -> \n''[1] masculino \n [2] Feminino'))
+sexo = int(input('Escolha seu sexo -> \n''[1] masculino \n [2] Feminino\n'
+                 'Opção Sexual: '))
 
 if media <=5 and sexo == 1:
     print('Foi reprovado!')
 
 elif media > 5 and media < 7 and sexo ==1:
     print('Você ficou de recuperação!')
-    nota3 = float(input('Digite a nota da prova de recuperação'))
+    nota3 = float(input('Digite a nota da prova de recuperação: '))
     mediaRec = (nota1+nota2+nota3)/3
     if mediaRec < 7:
         print('REPROVADO!!')
@@ -22,13 +23,15 @@ elif media > 7 and sexo ==1:
 
 elif media <=5 and sexo ==2:
     print('Você está reprovada!')
-elif media > 5 and media <7 and sexo==2
+elif media > 5 and media <7 and sexo==2:
     print('Você está de recuperação!')
-    nota3 = float(input('Digite a nota da prova de recuperação:'))
+    nota3 = float(input('Digite a nota da prova de recuperação: '))
     mediaRec = (nota1+nota2+nota3)/3
-    if mediaRec < 7:
-        print('Você está REPROVADA!!'
-    else media >= 7:
+    if mediaRec >= 7:
         print('Você está APROVADA!!\n'
               'Parabéns!!!')
+    else:
+        print('Você está REPROVADA!!')
+else:
+    print('Você está APROVADA!!')
 
