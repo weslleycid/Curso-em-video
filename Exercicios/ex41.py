0c88b5e2f1a32cead45929f0cfa0beec7277538d
Weslley Alves <weslleycid@gmail.com>
2022-12-18 21:14:13 -0300

    Correçoes para facilitar busca

---
+++ Exercicios/ex41.py
@@ -0,0 +1,17 @@
+from datetime import date
+anoNascimento = int(input('Digite o ano em que você nasceu: '))
+anoAtual = date.today().year
+idade = anoAtual - anoNascimento
+
+print('Você tem {} anos '.format(idade))
+
+if idade <= 9:
+    print('Sua categoria é MIRIM')
+elif idade >9 and idade <= 14:
+    print('Sua categoria é INFANTIL ')
+elif idade > 14 and idade <= 19:
+    print('Sua categoria é JUNIOR')
+elif idade> 19 and idade <=25:
+    print('Sua categoria é SÊNIOR')
+elif idade > 25:
+    print('Sua categoria é MASTER')
